40845c555a89a31463dae92eff6b18558e1da968
nadejda <bogdanovavenera30@gmail.com>
2021-02-20 05:42:53 +0300

    added carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,38 @@ $(document).ready(function(){
         //adaptiveHeight: true,
         prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        resposive: [
+        responsive: [
           {
             breakpoint: 992,
-            settings: {              
-              dots: true, 
-              arrows: false        
-            }
-          }
-          
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows: false,
+              dots: true,
+            },
+          },
         ]
-
-    });
+    })       
 });
 
