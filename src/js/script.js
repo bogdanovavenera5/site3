766ce0cb94658f9c0ba065f16dcb0de9fd3c6ab2
nadejda <bogdanovavenera30@gmail.com>
2021-02-23 10:41:03 +0300

    added modal window

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,       
         //adaptiveHeight: true,
-        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
           {
             breakpoint: 992,
@@ -51,12 +51,22 @@ $(document).ready(function(){
               $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
           })
       });
-  };
+    };
   
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
-    
-  });
   
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
 
-
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    })
+  });   
+});
